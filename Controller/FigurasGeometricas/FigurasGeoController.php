57d98d34314a83bfae0b93b60bf3b11bf9abba19
Julian Abril <jgabril@uniboyaca.edu.co>
2023-10-08 19:40:26 -0500

    segundo ejercicio

--- Controller/FigurasGeometricas/FigurasGeoController.php
+++ Controller/FigurasGeometricas/FigurasGeoController.php
@@ -1,23 +1,83 @@
 <?php
-namespace Pga\Controller\FigurasGeometricas;
-use Pga\Model\figurasgeom;
 
-class FigurasGeoController {
-    
-    function triangulo(){
-        if (isset($_POST['base']) && isset($_POST['altura'])) {
-            $base = floatval($_POST['base']);
-            $altura = floatval($_POST['altura']);
+namespace Controller\FigurasGeometricas\FigurasGeoController;
 
-            $figurasgeo = new FigurasGeom();
-            $figurasgeo->set('base', $base);
-            $figurasgeo->set('altura', $altura);
-            $operacion = ($figurasgeo->get('base') * $figurasgeo->get('altura')) / 2;
-            return $operacion;
-        } else {
-            return "Error: 'base' or 'altura' is not set in the POST data.";
-        }
+class TrianguloController
+{
+    public function mostrarFormulario()
+    {
+        echo '
+            <form method="post">
+                <label for="base">Base:</label>
+                <input type="number" name="base" id="base">
+                <label for="altura">Altura:</label>
+                <input type="number" name="altura" id="altura">
+                <input type="submit" value="Calcular Área">
+            </form>
+        ';
+    }
+
+    public function calcularArea($base, $altura)
+    {
+        return ($base * $altura) / 2;
     }
 }
 
-?>
+class CirculoController
+{
+    public function mostrarFormulario()
+    {
+        echo '
+            <form method="post">
+                <label for="radio">Radio:</label>
+                <input type="number" name="radio" id="radio">
+                <input type="submit" value="Calcular Área">
+            </form>
+        ';
+    }
+
+    public function calcularArea($radio)
+    {
+        return M_PI * $radio * $radio;
+    }
+}
+
+class CuadradoController
+{
+    public function mostrarFormulario()
+    {
+        echo '
+        <form method="post">
+            <label for="lado">Lado:</label>
+            <input type="number" name="lado" id="lado">
+            <input type="submit" value="Calcular Área">
+        </form>
+        ';
+    }
+
+    public function calcularArea($lado)
+    {
+        return $lado * $lado;
+    }
+}
+
+class RectanguloController
+{
+    public function mostrarFormulario()
+    {
+        echo '
+            <form method="post">
+            <label for="base_rect">Base:</label>
+            <input type="number" name="base_rect" id="base_rect">
+            <label for="altura_rect">Altura:</label>
+            <input type="number" name="altura_rect" id="altura_rect">
+            <input type="submit" value="Calcular Área">            
+            </form>
+        ';
+    }
+
+    public function calcularArea($base, $altura)
+    {
+        return $base * $altura;
+    }
+}
--- view/Ejercicio2.php
+++ view/Ejercicio2.php
@@ -1,77 +1,99 @@
 <?php
-include __DIR__ . '/../Model/Figurasgeo.php';
-include __DIR__ . '/../Controller/FigurasGeometricas/FigurasGeoController.php';
-use Pga\Controller\FigurasGeometricas\FigurasGeoController;
+require_once '/xampp/htdocs/Taller_pga/Controller/FigurasGeometricas/FigurasGeoController.php';
 
-$figurascontroller = new FigurasGeoController();
-$operacion = "";
+use Controller\FigurasGeometricas\FigurasGeoController\TrianguloController;
+use Controller\FigurasGeometricas\FigurasGeoController\CirculoController;
+use Controller\FigurasGeometricas\FigurasGeoController\CuadradoController;
+use Controller\FigurasGeometricas\FigurasGeoController\RectanguloController;
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST['triangulo'])) {
-        $operacion = $figurascontroller->triangulo();
+$trianguloController = new TrianguloController();
+$circuloController = new CirculoController();
+$cuadradoController = new CuadradoController();
+$rectanguloController = new RectanguloController();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['base']) && isset($_POST['altura'])) {
+        $base = floatval($_POST['base']);
+        $altura = floatval($_POST['altura']);
+        $resultado = $trianguloController->calcularArea($base, $altura);
+    } elseif (isset($_POST['radio'])) {
+        $radio = floatval($_POST['radio']);
+        $resultado = $circuloController->calcularArea($radio);
+    } elseif (isset($_POST['lado'])) {
+        $lado = floatval($_POST['lado']);
+        $resultado = $cuadradoController->calcularArea($lado);
+    } elseif (isset($_POST['base_rect']) && isset($_POST['altura_rect'])) {
+        $base = floatval($_POST['base_rect']);
+        $altura = floatval($_POST['altura_rect']);
+        $resultado = $rectanguloController->calcularArea($base, $altura);
     }
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
-    <title>Ejercicio 2</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="ejercicio2.css">
 </head>
 
 <body>
-<form class="main" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <a href="../index.php" class="eje">Volver</a>
+    <main class="main">
         <div class="container">
-            <div class="main_img"></div>
-            <section class="main_preview">
-                <div class="main_content">
-                    <h2 class="tittle">Triangulo</h2>
-                    <label for="base">Base (m):</label>
-                    <input type="number" name="base" step="0.1" required>
-                    <label for="altura">Altura (m):</label>
-                    <input type="number" name="altura" step="0.1" required>
-                    <input type="submit" name="triangulo" value="Operar">
-                    <p><?php echo $operacion; ?></p>
-                </div>
-            </section>
+            <h2 class="title">Triángulo</h2>
+            <div class="triangulo-bottom"></div>
+            <div class="container-figu">
+                <?php
+                $trianguloController->mostrarFormulario();
+                if (isset($resultado) && isset($_POST['base']) && isset($_POST['altura'])) {
+                    echo "Área del triángulo: $resultado";
+                }
+                ?>
+            </div>
         </div>
         <div class="container">
-            <div class="main_img"></div>
-            <section class="main_preview">
-                <div class="main_content">
-                    <h2 class="tittle">circulo</h2>
-                    <label for="lado">radio(m):</label>
-                    <input type="number" name="lado" step="0.1" require>
-                    <button type="submit">operar</button>
-                </div>
-            </section>
+            <h2 class="title">Circulo</h2>
+            <div class="circulo-bottom"></div>
+            <div class="container-figu">
+                <?php
+                $circuloController->mostrarFormulario();
+                if (isset($resultado) && isset($_POST['radio'])) {
+                    echo "Área del círculo: $resultado";
+                }
+                ?>
+            </div>
         </div>
         <div class="container">
-            <div class="main_img"></div>
-            <section class="main_preview">
-                <div class="main_content">
-                    <h2 class="tittle">cuadrado</h2>
-                    <label for="lado">lado(m):</label>
-                    <input type="number" name="lado" step="0.1" require>
-                    <button type="submit">operar</button>
-                </div>
-            </section>
+            <h2 class="title">Cuadrado</h2>
+            <div class="cuadrado-bottom"></div>
+            <div class="container-figu">
+                <?php
+                $cuadradoController->mostrarFormulario();
+
+                if (isset($resultado) && isset($_POST['lado'])) {
+                    echo "Área del Cuadrado: $resultado";
+                }
+                ?>
+            </div>
         </div>
         <div class="container">
-            <div class="main_img"></div>
-            <section class="main_preview">
-                <div class="main_content">
-                    <h2 class="tittle">rectangulo</h2>
-                    <label for="lado">altura(m):</label>
-                    <input type="number" name="lado" step="0.1" require>
-                    <label for="base">base(m):</label>
-                    <input type="number" name="base"step="0.1" require>
-                    <button type="submit">operar</button>
-                </div>
-            </section>
+            <h2 class="title">Rectángulo</h2>
+            <div class="rectangulo-bottom"></div>
+            <div class="container-figu">
+                <?php
+                $rectanguloController->mostrarFormulario();
+
+                if (isset($resultado) && isset($_POST['base_rect']) && isset($_POST['altura_rect'])) {
+                    echo "Área del Rectángulo: $resultado";
+                }
+                ?>
+            </div>
         </div>
-    </form>
+    </main>
 </body>
 
 </html>
