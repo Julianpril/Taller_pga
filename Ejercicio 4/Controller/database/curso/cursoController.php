68ca5cdfd9569e8038643ae48edf70457503c288
AndrewEsp22 <asferrucho@uniboyaca.edu.co>
2023-10-14 15:11:16 -0500

    estilos

--- Ejercicio 4/Controller/database/curso/cursoController.php
+++ Ejercicio 4/Controller/database/curso/cursoController.php
@@ -8,7 +8,7 @@ class CursoController extends EntityController{
 
     function allData()
     {
-        $sql
+    
     }
 }
 ?>
--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         <a href="view/ejercicio1.php" class="nav-item color1">Ejercicio 1</a>
         <a href="view/ejercicio2.php" class="nav-item color2">Ejercicio 2</a>
         <a href="view/ejercicio3.php" class="nav-item color3">Ejercicio 3</a>
-        <a href="" class="nav-item color4">Ejercicio 4</a>
+        <a href="Ejercicio 4/View/ejercicio4.php" class="nav-item color4">Ejercicio 4</a>
     </nav>
 </body>
 
