87a9c4d8d57844b1e3d0fbdbaf8261df87367563
AndrewEsp22 <asferrucho@uniboyaca.edu.co>
2023-10-26 15:17:16 -0500

    Ultimos arreglo de estilos

--- index.php
+++ index.php
@@ -8,13 +8,26 @@
 </head>
 
 <body>
+<div class="container">
+    <h2>TOP SCORE </h2>
+    <div class="image-container">
+        <img class="img01" src="recursos/Andres01.png">
+        <h4>Andres</h4>
+    </div>
+    <div class="image-container">
+        <img class="img02" src="recursos/Julian01.png">
+        <h4>Julian</h4>
+    </div>
+</div>
+
     <h1 class="title">EJERCICIOS</h1>
-    <nav class="nav">
+     <nav class="nav">
         <a href="Ejercicio1/view/ejercicio1.php" class="nav-item color1">Ejercicio 1</a>
         <a href="Ejercicio2/view/ejercicio2.php" class="nav-item color2">Ejercicio 2</a>
         <a href="Ejercicio3/view/ejercicio3.php" class="nav-item color3">Ejercicio 3</a>
         <a href="Ejercicio4/View/Docentes/docentes.php" class="nav-item color4">Ejercicio 4</a>
     </nav>
+    
 </body>
 
 </html>
